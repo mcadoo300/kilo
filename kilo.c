2812fb09674a1dc42bb807843add762ddb8e3ebf
marc <mcadoo300@gmail.com>
2024-01-16 19:21:18 -0500

    up to step 32

--- kilo.c
+++ kilo.c
@@ -65,6 +65,29 @@ char editorReadKey() {
   return c;
 }
 
+int getCursorPosition(int *rows, int *cols) {
+  char buf[32];
+  unsigned int i = 0;
+
+  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+  printf("\r\n");
+
+
+  while (i < sizeof(buf)-1) {
+    if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+    if (buf[i] == 'R') break;
+    i++; 
+  }
+  buf[i] = '\0';
+
+  printf("\r\n&buf[1]: '%s'\r\n", &buf[1]);
+
+  editorReadKey();
+
+  return -1;
+}
+
 
 int getWindowSize(int *rows, int *cols){
   struct winsize ws;
@@ -72,7 +95,7 @@ int getWindowSize(int *rows, int *cols){
   if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
     if (write(STDOUT_FILENO,"\x1b[999C\x1b[999B", 12) != 12) return -1;
     editorReadKey();
-    return -1;
+    return getCursorPosition(rows,cols);
   } else {
     *cols = ws.ws_col;
     *rows = ws.ws_row;
