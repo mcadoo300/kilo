49f97acf96fdb44d06870cb0625e31a69be23e80
marc <mcadoo300@gmail.com>
2024-01-18 15:42:45 -0500

    some

--- kilo.c
+++ kilo.c
@@ -16,6 +16,7 @@
 /*DATA*/
 
 struct editorConfig {
+  int cx, cy;
   int screenrows;
   int screencols;
   struct termios orig_termios;
@@ -164,8 +165,10 @@ void editorRefreshScreen() {
   abAppend(&ab,"\x1b[H",3);
 
   editorDrawRows(&ab);
-
-  abAppend(&ab, "\x1b[H",3);
+  
+  char buf[32];
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx +1);
+  abAppend(&ab, buf,strlen(buf));
   abAppend(&ab, "\x1b[?25h",6);
 
   write(STDOUT_FILENO,ab.b,ab.len);
@@ -174,6 +177,24 @@ void editorRefreshScreen() {
 
 
 /*INPUT*/
+
+void editorMoveCursor(char key) {
+  switch (key) {
+    case 'a':
+      E.cx--;
+      break;
+    case 'd':
+      E.cx++;
+      break;
+    case 'w':
+      E.cy--;
+      break;
+    case 's':
+      E.cy++;
+      break;
+  }
+}
+
 
 void editorProcessKeypress() {
   char c = editorReadKey();
@@ -184,12 +205,22 @@ void editorProcessKeypress() {
       write(STDOUT_FILENO,"\x1b[H",3);
       exit(0);
       break;
+
+    case 'w':
+    case 's':
+    case 'a':
+    case 'd':
+      editorMoveCursor(c);
+      break;
   }
 }
 
 /*INIT*/
 
 void initEditor() {
+  E.cx = 0;
+  E.cy = 0;
+
   if (getWindowSize(&E.screenrows,&E.screencols) == -1) die("getWindowSize");
 }
 
