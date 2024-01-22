884ced588414e4126504b56c1351c8017130bcc5
marc <mcadoo300@gmail.com>
2024-01-22 12:33:17 -0500

    save as fully implemented. up to step 130. Next is search

--- kilo.c
+++ kilo.c
@@ -344,7 +344,11 @@ void editorOpen(char *filename) {
 
 void editorSave() {
   if (E.filename == NULL) {
-    E.filename = editorPrompt("Save as: %s");
+    E.filename = editorPrompt("Save as: %s (ESC to cancel)");
+    if (E.filename == NULL){
+      editorSetStatusMessage("Save aborted");
+      return;
+    }
   }
 
   int len;
@@ -517,7 +521,13 @@ char *editorPrompt(char *promt){
     editorRefreshScreen();
 
     int c = editorReadKey();
-    if (c == '\r'){
+    if (c == DEL_KEY || c == CTRL_KEY('h') || c == BACKSPACE){
+      if (buflen != 0) buf[--buflen] = '\0';
+    }else if (c == '\x1b'){
+      editorSetStatusMessage("");
+      free(buf);
+      return NULL;
+    } else if (c == '\r'){
       if (buflen != 0) {
         editorSetStatusMessage("");
         return buf;
