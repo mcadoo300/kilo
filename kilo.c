68bfc0ee85454adaaef84e3094cc4c00e3e3b419
marc <mcadoo300@gmail.com>
2024-01-22 10:38:01 -0500

    safe exit after making changes

--- kilo.c
+++ kilo.c
@@ -22,7 +22,7 @@
 #define KILO_VERSION "0.0.1"
 #define CTRL_KEY(k) ((k) & 0x1f)
 #define KILO_TAB_STOP 8
-
+#define KILO_QUIT_TIMES 3
 
 enum editorKey {
   BACKSPACE = 127,
@@ -478,6 +478,8 @@ void editorMoveCursor(int key) {
 
 
 void editorProcessKeypress() {
+  static int quit_times = KILO_QUIT_TIMES;
+
   int c = editorReadKey();
   switch (c) {
     case '\r':
@@ -485,6 +487,12 @@ void editorProcessKeypress() {
       break;
 
     case CTRL_KEY('q'):
+      if (E.dirty && quit_times > 0){
+        editorSetStatusMessage("WARNING!!! File has unsaved changes. "
+                               "Press Ctrl-Q %d more times to quit.", quit_times);
+        quit_times--;
+        return;
+      }
       write(STDOUT_FILENO, "\x1b[2J", 4);
       write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
@@ -537,6 +545,7 @@ void editorProcessKeypress() {
       editorInsertChar(c);
       break;
   }
+  quit_times = KILO_QUIT_TIMES;
 } 
 
 /*INIT*/
