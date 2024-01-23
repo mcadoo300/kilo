5d39637baa2dc0bcde52f20b46a9bd955fb6a547
marc <mcadoo300@gmail.com>
2024-01-22 20:35:38 -0500

    search feature complete

--- kilo.c
+++ kilo.c
@@ -388,16 +388,38 @@ void editorSave() {
 
 
 void editorFindCallback(char *query, int key){
+  static int last_match = -1;
+  static int direction = -1;
+
+
+
   if (key == '\r' || key == '\x1b'){
+    last_match = -1;
+    direction = 1;
     return;
+  } else if (key == ARROW_RIGHT || key == ARROW_DOWN){
+    direction =1;
+  }else if (key == ARROW_LEFT || key == ARROW_UP){
+    direction =-1;
+  } else {
+    last_match = -1;
+    direction = 1;
   }
 
+
+  if (last_match == -1) direction = 1;
+  int current = last_match;
   int i;
   for ( i = 0; i < E.numrows; i++){
-    erow *row = &E.row[i];
+    current += direction;
+    if (current == -1) current = E.numrows -1;
+    else if (current == E.numrows) current = 0;
+
+    erow *row = &E.row[current];
     char *match = strstr(row->render, query);
-    if (match) { 
-      E.cy = i;
+    if (match) {
+      last_match = current;
+      E.cy = current;
       E.cx = editorRowRxToCx(row, match - row->render); 
       E.rowoff = E.numrows;
       break;
@@ -413,7 +435,7 @@ void editorFind(){
   int saved_rowoff = E.rowoff;
 
 
-  char *query = editorPrompt("Search: %s (ESC to cancel)", editorFindCallback);
+  char *query = editorPrompt("Search: %s (ESC to cancel & ARROWS to navigate & ENTER to select)", editorFindCallback);
   if (query){
     free(query);
   } else {
